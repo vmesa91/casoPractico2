90b5bf7e92633419896bf2c762afdeeee0a00ea3
Virginia <v.mesagetafe@gmail.com>
2021-07-18 22:38:46 +0200

    Add all files

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -5,3 +5,14 @@ ansible-playbook -i hosts config-init.yaml
 
 echo "2. Install NFS in the server"
 ansible-playbook -i hosts -l masters install-nfs.yaml 
+
+echo "3. Commons Tasks to do in all nodes"
+ansible-playbook -i hosts commons-tasks.yaml
+
+echo "4. Configure Master Node & Install Ingress Controller"
+ansible-playbook -i hosts -l masters configuration-master.yaml
+
+echo "5. Configure Workers Node"
+ansible-playbook -i hosts -l masters configuration-worker.yaml
+
+echo "6. Pending.....DEPLOY APP"
