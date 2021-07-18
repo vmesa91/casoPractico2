773f49acd36bd987c0dc006185596e6c1599cc9c
Vir <v.mesagetafe@gmail.com>
2021-07-18 16:37:30 +0000

    Eliminados ficheros de cache

--- terraform/credentials.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "17ec4094-53e3-421c-9f29-b98a61b8b866"
-  client_id       = "540e7c21-eeac-4e7a-bd2e-9173f0c48606"
-  client_secret   = "y7jagLZDBI.8AunAw~2lM4jQS58A.6Pr9M"
-  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
-}
